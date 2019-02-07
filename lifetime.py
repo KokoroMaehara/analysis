becf5d753fe80a03d4e6256311d7a1df1b9ac4c5
KokoroMaehara <kmaehara@hepl.phys.nagoya-u.ac.jp>
2019-02-07 21:54:28 +0900

    Bug fix
    In PyROOT, use '.' instead of '->'
    e.g.)
    ROOT: tree -> Draw()
    PyROOT: tree.Draw()

--- lifetime.py
+++ lifetime.py
@@ -3,17 +3,19 @@ from ROOT import gStyle
 
 gStyle.SetOptFit()
 
-f_in = pR.TFile("run0124_1.root")
+file = TFile("run0124_1.root")
 
 c1 = TCanvas("c1","ch1",200,100,800,600)
 
 f1 = TF1("f1","[0]*exp(-x/[1])+[2]")
-f1 = SetNpx(10000)
-f1 = SetParameters(1,2200,0)
-f1 = SetParNames("constant","#tau","offset")
+f1.SetNpx(10000)
+f1.SetParameters(1,2200,0)
+f1.SetParNames("constant","#tau","offset")
 
-v1290 = Draw("0.002441*(tdc-trig)","ch==1")
-h = Fit("f1","","",-8000,-5000)
-f1 = Draw("same")
+tree = file.Get("v1290")
+tree.Draw("0.002441*(tdc-trig)","ch==1")
+h = TH1F("h","tdc(ch1)",10000,-10000,0)
+h.Fit("f1","","",-8000,-5000)
+f1.Draw("same")
 
-c1 = Print("run1.png")
+c1.Print("run1.png")
