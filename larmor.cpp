1a31f1dfd7333573b3a5995cc0cfd6af10c36c60
KokoroMaehara <kmaehara@hepl.phys.nagoya-u.ac.jp>
2019-02-22 21:10:55 +0900

    Bug fix: TMath::(Capital)...

--- larmor.cpp
+++ larmor.cpp
@@ -9,7 +9,7 @@
      //h1->SetStats(0);                                    //統計ボックスの非表示
      v1290->Draw("0.02441*tdc", "ch==1"); //h(指定範囲の分割数,左端,右端)
      double x;
-     TF1 *f1 = new TF1("f1", "[0]*(TMath::exp(-x/[1])*(1+[2]*TMath::cos([3]*x+[4]))+TMath::exp(-(x-[5])/[1])*(1+[2]*TMath::cos([3]*(x-[5])+[4])))", 3000, 8000); //フィッティングする為の関数の定義。[]で囲んである部分がフィットで変化させるパラメーター。関数を使うときはTMath::expのようにする。TMath::Gaus(Double_t x,Double_t mean,Double_t sigma)
+     TF1 *f1 = new TF1("f1", "[0]*(TMath::Exp(-x/[1])*(1+[2]*TMath::Cos([3]*x+[4]))+TMath::Exp(-(x-[5])/[1])*(1+[2]*TMath::Cos([3]*(x-[5])+[4])))", 3000, 8000); //フィッティングする為の関数の定義。[]で囲んである部分がフィットで変化させるパラメーター。関数を使うときはTMath::expのようにする。TMath::Gaus(Double_t x,Double_t mean,Double_t sigma)
      f1->SetNpx(100000);                                                                                                                                         //フィットした関数をプロットする細かさ
 
      f1->SetParameters(1, 2196.9811, 0.5, 0.013, 0, 598); //パラメータの初期値を決める
