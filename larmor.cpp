423305f175a2de094482aa9f63ba1dda8fda5daf
KokoroMaehara <kmaehara@hepl.phys.nagoya-u.ac.jp>
2019-02-22 22:12:56 +0900

    Delete statistic box

--- larmor.cpp
+++ larmor.cpp
@@ -6,7 +6,7 @@
      TFile *_file0 = TFile::Open("merge1_29.root");                   //TFile::Open("xxx.yyy"):ファイル"xxx.yyy"を開く
 
      TH1F *h1 = new TH1F("h1", "", 2000, 0, 10000);
-     //h1->SetStats(0);                                    //統計ボックスの非表示
+     h1->SetStats(0);                                         //統計ボックスの非表示
      v1290->Draw("0.02441*(tdc-trig)+10000 >> h1", "ch==15"); //h(指定範囲の分割数,左端,右端)
      double x;
      TF1 *f1 = new TF1("f1", "[0]*(TMath::Exp(-x/[1])*(1+[2]*TMath::Cos([3]*x+[4]))+TMath::Exp(-(x-[5])/[1])*(1+[2]*TMath::Cos([3]*(x-[5])+[4])))+[6]", 2000, 8000); //フィッティングする為の関数の定義。[]で囲んである部分がフィットで変化させるパラメーター。関数を使うときはTMath::expのようにする。TMath::Gaus(Double_t x,Double_t mean,Double_t sigma)
